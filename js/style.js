de4153d94cbc4e7069ef2ab14fd0dfda66d73c21
clintch10 <clintch10@gmail.com>
2021-10-13 14:02:22 +0800

    Navbar Done

---
+++ js/style.js
@@ -0,0 +1,31 @@
+/* Show menu */
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+/* Close menu when click a link or outside the menu */
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+/* Close menu when clicked outside the container */
+const hamburgerClose = document.getElementById('hamburger')
+const navMenuClose = document.getElementById('nav-menu')
+
+document.onclick = function(e){
+    if(e.target.id !== 'hamburger' && e.target.id !== 'nav-menu')
+    {
+        closeMenu();
+    }
+}
